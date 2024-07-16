335c493bc4da810f5a0386dc10e859aa57736fc1
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-07-16 10:15:47 +0530

    16-07

--- components/smoothScroll/SmoothScroll.jsx
+++ components/smoothScroll/SmoothScroll.jsx
@@ -1,23 +1,24 @@
-"use client";
+"use client"
 import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
 
-export const lenis = new Lenis({
-    smoothWheel: true,
-    duration: 1,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  });
-  
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-  
-  requestAnimationFrame(raf);
 
-const SmoothScroll = ( {children}) => {
+const SmoothScroll = ({ children }) => {
   useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      duration: 2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
     requestAnimationFrame(raf);
+    return () => {
+      lenisRef.current?.destroy();
+    };
   }, []);
   return (
     <>
