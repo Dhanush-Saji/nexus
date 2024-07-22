27cf0e718f87a9559cf584afef14073554f1db3d
Dhanush Saji <dhanushsaji@Dhanushs-Laptop.local>
2024-07-23 00:29:56 +0530

    23-07

--- app/(with-topbar)/chats/[chatId]/page.jsx
+++ app/(with-topbar)/chats/[chatId]/page.jsx
@@ -6,8 +6,8 @@ import { getServerSession } from "next-auth";
 const Page = async({params:{chatId}}) => {
   const session = await getServerSession(authOptions)
   return (
-    <main className="flex items-center w-full flex-col relative h-[100vh] px-6">
-      <div className=" w-full bg-[#111827] rounded-[10px] border border-gray-50/10 h-[78vh] md:h-[85vh] mt-[12vh]">
+    <main className="flex items-center w-full flex-col relative h-[100vh] px-6  pt-[12vh]">
+      <div className=" w-full bg-[#111827] rounded-[10px] border border-gray-50/10 h-[78vh] md:h-[85vh]">
       <MobChatSection chatId={chatId} session={session} />
       </div>
       </main>
