1b97048d95b66ab837a57e6ce6f751f5fa711312
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-07-30 10:43:03 +0530

    30-07

--- components/section/HeroSection/HeroSection.jsx
+++ components/section/HeroSection/HeroSection.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
               )}
             >
               <AnimatedShinyText className="customBorder2 backdrop-blur-sm inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-                <span className='gradient1 bg-clip-text'>✨ new update 2.1</span>
+                <span className='gradient1 bg-clip-text'>✨ Introducing Chat Platform</span>
               </AnimatedShinyText>
             </div>
           </div>
